526ef732f62130a5a7b5de2c62cf7b70fc2f5110
Max <pj4316@naver.com>
2018-10-22 15:39:46 +0900

    1. OnTouch()에서 멀티 터치 인식 2. CountDownTimer로 추첨기능 구현 3. MultiTouch 인식 4. View 객체 이미지 Scale 변동 추가 5. 추첨 후의 결과에 대한 그래픽 표시 ( canvas.clipPath(path, Region.Op.DIFFERENCE) ) - 해당 Path를 제외한 반대영역 그리기

--- app/src/main/java/com/pickme/anychooser/TouchView.java
+++ app/src/main/java/com/pickme/anychooser/TouchView.java
@@ -7,26 +7,22 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.RectF;
 import android.graphics.Region;
-import android.os.Build;
 import android.os.CountDownTimer;
 import android.os.Vibrator;
 import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class TouchView extends View {
 
+    // 포인터의 위치 및 개별 Radius, 색깔을 저장
+    // Radius를 점진적으로 키우기 위한 Timer
     class Pointer{
         float cX;
         float cY;
@@ -67,68 +63,88 @@ public class TouchView extends View {
 
     }
 
+    // 진동을 위한 객체
     Vibrator vibrator = null;
-    SparseArray<Pointer> pointers = null;
-    int color_i = 0;
-    int[] COLOR = {Color.WHITE, Color.RED, Color.CYAN, Color.BLUE, Color.YELLOW, Color.GREEN, Color.GRAY, Color.DKGRAY, Color.MAGENTA, Color.BLACK};
-    String[] COLORS = {"흰색", "빨강색", "하늘색", "파랑색", "노랑색", "초록색", "회색", "진한 회색", "자주색", "검정색"};
-    float max_radius = 200;
 
+    // Pointer를 PointerId별로 저장하는 배열
+    SparseArray<Pointer> pointers = null;
+
+    // 색변경을 위한 인덱스
+    int color_i = 0;
+
+    // 색깔 테이블 및 한글표시
+    int[] COLOR = {Color.WHITE, Color.RED, Color.CYAN, Color.BLUE, Color.YELLOW, Color.GREEN, Color.GRAY, Color.DKGRAY, Color.MAGENTA, Color.TRANSPARENT};
+    String[] COLORS = {"흰색", "빨강색", "하늘색", "파랑색", "노랑색", "초록색", "회색", "진한 회색", "자주색", "검정색"};
+
+    // 추첨을 위한 Timer
     CountDownTimer timer = null;
 
+    // 그리기 위한 캔버스
     Canvas canvas = null;
 
-    ValueAnimator mTimerAnimator = null;
-    float mCircleSweepAngle;
-
+    // 추첨 결과여부를 위한 Flag
     boolean finished = false;
 
     public TouchView(Context context) {
         super(context);
+
+        init();
+    }
+
+    // 각종 뷰에 필요한 객체 초기화
+    public void init(){
+        //뷰생성에서 바탕화면 색을 검정으로 설정한다
         this.setBackgroundColor(Color.BLACK);
 
         pointers = new SparseArray<>();
-
         canvas = new Canvas();
 
+        // 아무런 포인터 추가 제거가 없는 상태에서, 3초간 동작하며
+        // 이후에 결과를 표시
         timer = new CountDownTimer(3000, 50) {
             @Override
             public void onTick(long millisUntilFinished) {
 
             }
 
+            //추첨 결과를 처리하는 루틴
             @Override
             public void onFinish() {
+
+                // Pointers의 갯수가 0일 경우 처리하지 않음
                 if(pointers.size() == 0) return;
 
+                // 랜덤 숫자를 하나 얻어서 포인터 인덱스를 고른다
                 Random random = new Random();
                 int index = Math.abs(random.nextInt()) % pointers.size();
 
+                // 추첨이 완료 되었기 때문에 진동으로 사용자에게 결과를 알린다
                 vibrator.vibrate(1000);
 
-                Pointer p = pointers.get(index);
+                // Index를 통해 Pointer를 얻는다
+                Pointer p = pointers.get(pointers.indexOfKey(index));
 
                 if(p != null){
                     //pointers.clear();
 
+
+                    //추첨된 Pointer를 제외한 나머지의 객체의 색깔을 검정으로 변경
                     for(int i=0;i<pointers.size();i++)
                     {
                         if(i != index) pointers.get(pointers.indexOfKey(i)).color = COLOR.length-1;
                     }
 
+                    // 추첨결과를 True로 표시
                     finished = true;
 
+                    // 당첨된 결과를 화면에 글씨로 출력해준다
                     Toast.makeText(getContext(), COLORS[pointers.get(index).color]+" 당첨!", Toast.LENGTH_SHORT).show();
-
-                    float untiltoDraw = p.radius + 50;
-
                 }
 
 
             }
         };
     }
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
@@ -158,6 +174,7 @@ public class TouchView extends View {
 
                 for(int i=0;i<event.getPointerCount();i++) {
                     int pId = event.getPointerId(i);
+
                     pointers.get(pId).setcX(event.getX(i));
                     pointers.get(pId).setcY(event.getY(i));
                 }
@@ -192,7 +209,7 @@ public class TouchView extends View {
 
         Paint paintCircle = new Paint();
         paintCircle.setAntiAlias(true);
-        paintCircle.setStyle(Paint.Style.FILL);
+        paintCircle.setStyle(Paint.Style.FILL_AND_STROKE);
 
         Path path = new Path();
         Paint paintPath = new Paint();
@@ -204,7 +221,7 @@ public class TouchView extends View {
 
             canvas.drawCircle(p.cX,p.cY,p.radius,paintCircle);
 
-            if(COLOR[p.color] != Color.BLACK) path.addCircle(p.cX,p.cY,p.radius + 40, Path.Direction.CW);
+            if(COLOR[p.color] != Color.BLACK) path.addCircle(p.cX,p.cY,p.radius+50, Path.Direction.CW);
             else path.reset();
 
             if(finished){
@@ -220,24 +237,4 @@ public class TouchView extends View {
     public void setVibrator(Vibrator v){
         vibrator = v;
     }
-
-    public void start(int secs) {
-        mTimerAnimator = ValueAnimator.ofFloat(0f, 1f);
-        mTimerAnimator.setDuration(TimeUnit.SECONDS.toMillis(secs));
-        mTimerAnimator.setInterpolator(new LinearInterpolator());
-        mTimerAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                drawProgress((float) animation.getAnimatedValue());
-            }
-        });
-
-        mTimerAnimator.start();
-    }
-
-    private void drawProgress(float progress) {
-        mCircleSweepAngle = 360 * progress;
-        invalidate();
-    }
-
 }
